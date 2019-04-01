5eb880bc3979fca244b42da622e86fa38aa07140
tspeaking <justtalk@foxmail.com>
2019-04-01 20:21:16 +0800

    finished two simple pages

--- llogs/urls.py
+++ llogs/urls.py
@@ -1,8 +1,12 @@
+'''Urls for llogs app'''
+
 from django.conf.urls import url
 
 from . import views
 
 app_name = 'llogs'
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^topics/$', views.topics, name='topics'),
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
 ]
--- llogs/views.py
+++ llogs/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Topic
+
 def index(request):
     return render(request, 'llogs/index.html')
+
+def topics(request):
+    '''Display all topics'''
+    topics = Topic.objects.order_by('time_added')
+    context = {'topics': topics}
+    return render(request, 'llogs/topics.html', context)
+
+def topic(request, topic_id):
+    '''Display memos for a spcific topic'''
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-time_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'llogs/topic.html', context)
